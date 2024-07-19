7b1781eb04bd7f048a46c83d19ead8255bb930c9
qiufeng <51955656+qiuf2004@users.noreply.github.com>
2024-07-19 15:04:32 +0800

    Add files via upload

---
+++ examples/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@linewell/button';
+import React from 'react';
+
+const Demo = () => {
+  return (
+    <div>
+      <Button.Add></Button.Add>
+      <Button.Delete></Button.Delete>
+      <Button.Back></Button.Back>
+      <Button.Save></Button.Save>
+      <Button.Confirm></Button.Confirm>
+    </div>
+  );
+};
+
+export default Demo;
